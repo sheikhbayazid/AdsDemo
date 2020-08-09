acd3db46101c048cc45e2da2f763e8f7d177ea61
sheikhabayzid <sheikhbayazid@gmail.com>
2020-08-09 16:53:57 +0600

    Initial Commit

---
+++ AdsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdsDemo
+//
+//  Created by Sheikh Bayazid on 8/9/20.
+//  Copyright © 2020 Sheikh Bayazid. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
